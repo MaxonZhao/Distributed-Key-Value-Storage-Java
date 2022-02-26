3b99e9bf9d37b74c535391bf55ea1f6e736e9bd8
MaxonZhao <maxonzhao@gmail.com>
2022-02-26 10:56:39 -0800

    chore: update hashcricle test

---
+++ src/test/java/com/g10/cpen431/a7/HashCircleTest.java
@@ -0,0 +1,24 @@
+package com.g10.cpen431.a7;
+
+import com.g10.util.Hash;
+import com.g10.util.HashCircle;
+import com.g10.util.NodeInfo;
+import org.junit.jupiter.api.Test;
+
+import java.net.InetSocketAddress;
+
+class HashCircleTest {
+//    @Test
+//    void hashCircleTest() {
+//        NodeInfo.initializeNodesList("src/main/java/com/g10/util/serverList.yml", 1);
+//
+//        HashCircle hc = HashCircle.getInstance();
+//        byte[] index = {5,8,2,6,3,9,5,8, 4};
+//        System.out.println("-------------");
+//        int size = index.length;
+////        for(int i=0;i<size;i++){
+//            InetSocketAddress server = hc.findNodeFromHash(index);
+//            System.out.println("index="+index +" hash="+ Hash.hash(index) +" server=" + server.getHostName());
+//    }
+//        hc.findNodeFromHash()
+}
