0b5cf07be5a668e69dd9e49344bf16c29bdf9bb4
MaxonZhao <maxonzhao@gmail.com>
2022-02-28 19:36:41 -0800

    bugfix: get rid of descendingMap

--- src/main/java/com/g10/util/HashCircle.java
+++ src/main/java/com/g10/util/HashCircle.java
@@ -112,7 +112,7 @@ public class HashCircle {
         }
 
         /* get potential nodes before the hashed key in 'clockwise' order */
-        potentialNodes = nodesTreeMap.headMap(hash, true).descendingMap();
+        potentialNodes = nodesTreeMap.headMap(hash, true);
 
         for (Map.Entry<Long, InetSocketAddress> node : potentialNodes.entrySet()) {
             updateNodesStatus();
